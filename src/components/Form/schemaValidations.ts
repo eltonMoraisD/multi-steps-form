c9d525a64358e0a5efc2861dc3d9d2ce9235d140
Elton <admin@Admins-MBP-2.fritz.box>
2023-11-05 13:03:50 +0100

    responsive header

--- src/components/Form/schemaValidations.ts
+++ src/components/Form/schemaValidations.ts
@@ -3,13 +3,13 @@ import { z } from "zod";
 const phoneRegex = new RegExp(/^\+\d{3} \d{3} \d{3} \d{3}$/);
 
 export const userSchema = z.object({
-  firstName: z.string().min(1, "Type your first name").max(30),
-  lastName: z.string().min(1, "Type your last name").max(30),
-  email: z.string().min(1, "Type your e-mail").email("Invalid email"),
-  address: z.string().min(1, "Type your address"),
-  city: z.string().min(1, "Type your city"),
-  postcode: z.string().min(1, "Type your code"),
-  country: z.string().min(1, "Type your country"),
+  firstName: z.string().min(1, "*required").max(30),
+  lastName: z.string().min(1, "*required").max(30),
+  email: z.string().min(1, "*required").email("*Invalid email !"),
+  address: z.string().min(1, "*required"),
+  city: z.string().min(1, "*required"),
+  postcode: z.string().min(1, "*required"),
+  country: z.string().min(1, "*required"),
   phone: z.string().regex(phoneRegex, "Invalid country Number"),
   accountType: z
     .string()
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,11 +4,15 @@ import logo from '../../assets/logo.png'
 
 export default function Header() {
   return (
-    <header className='w-full bg-slate-100 flex items-center justify-center'>
-      <div className='container mx-auto'>
-        <div className='w-full text-center'>
-          <Image className='mx-auto mt-2' src={logo} width={100} height={100} alt={''} />
-          <p className='m-6 lg:text-4xl md:text-2xl sm: text-lg font-semibold text-blue-900'>Multi step form</p>
+    <header className='w-full xm:h-40 bg-slate-100 flex items-center justify-center'>
+      <div className='container justify-center  items-center mx-auto'>
+        <div className='w-full text-center '>
+          <div className=' lg:flex text-center p-10 lg:items-center lg:justify-center lg:text-center sm:flex sm:items-center sm:justify-between sm:text-center'>
+            <Image className='mt-2 xm:mt-10  xm:w-[80px] xm:h-[90px] ' src={logo} alt='logo' />
+            <p className='m-6  xm:mb-10 lg:text-4xl lg:mt-16 md:mt-16 md:text-2xl sm:mt-14 sm:text-lg font-semibold text-blue-900'>
+              Multi step form
+            </p>
+          </div>
         </div>
       </div>
     </header>
